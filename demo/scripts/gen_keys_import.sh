2ce11795a3de3232e52358792943f6f0bb0975fd
Charles Moulliard <ch007m@gmail.com>
2015-09-22 19:48:20 +0200

    Update script

--- demo/scripts/gen_keys_import.sh
+++ demo/scripts/gen_keys_import.sh
@@ -3,6 +3,11 @@
 rm -rf gerrit-admin-ssh
 rm -rf gerrit-users-ssh-keys
 
+oc delete secrets gerrit-admin-ssh
+oc delete secrets gerrit-users-ssh-keys
+oc delete secrets jenkins-git-ssh
+oc delete secrets jenkins-master-ssh
+
 mkdir gerrit-admin-ssh
 cd gerrit-admin-ssh/
 ssh-keygen -b 4096 -t rsa -f ssh-key -q -N "" -C "admin@fabric8.io"
@@ -14,4 +19,5 @@ ssh-keygen -b 4096 -t rsa -f jenkins-master-ssh -q -N "" -C "jenkins@fabric8.io"
 ssh-keygen -b 4096 -t rsa -f sonarqube-ssh -q -N "" -C "sonar@fabric8.io"
 cd ..
 
-gofabric8 secrets
+
+gofabric8 secrets -y
