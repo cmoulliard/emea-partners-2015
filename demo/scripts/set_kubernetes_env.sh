e6ac9ec3d21daeac1fd55081bfe5f2831bc50908
Charles Moulliard <ch007m@gmail.com>
2015-09-23 17:22:11 +0200

    Pass IPADDRESS as param

--- demo/scripts/set_kubernetes_env.sh
+++ demo/scripts/set_kubernetes_env.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+HOST=$1
+# vagrant.f8
+
 unset DOCKER_CERT_PATH
 unset DOCKER_TLS_VERIFY
-export DOCKER_HOST=tcp://vagrant.f8:2375
+export DOCKER_HOST=tcp://$HOST:2375
 export KUBERNETES_NAMESPACE=default
-export KUBERNETES_MASTER=https://vagrant.f8:8443
+export KUBERNETES_MASTER=https://$HOST:8443
 export KUBERNETES_DOMAIN=vagrant.f8
 export KUBERNETES_TRUST_CERT="true"
