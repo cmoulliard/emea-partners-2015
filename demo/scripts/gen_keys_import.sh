ffcf2fd6d36b0e7815f012a05e5d8bdc99226283
Charles Moulliard <ch007m@gmail.com>
2015-09-23 10:59:17 +0200

    Update script to generate the keys, import them

--- demo/scripts/gen_keys_import.sh
+++ demo/scripts/gen_keys_import.sh
@@ -3,25 +3,26 @@
 rm -rf gerrit-admin-ssh
 rm -rf gerrit-users-ssh-keys
 
-echo "Delete secrets in oc"
-oc project default
+oc login -u admin -p admin https://172.28.128.4:8443
+
+echo ">> Delete secrets in oc"
 oc delete secrets gerrit-admin-ssh
 oc delete secrets gerrit-users-ssh-keys
 oc delete secrets jenkins-git-ssh
 oc delete secrets jenkins-master-ssh
 
-echo "Gen keys for admin" 
+echo ">> Generate new keys for admin"
 mkdir gerrit-admin-ssh
 cd gerrit-admin-ssh/
-ssh-keygen -b 4096 -t rsa -f ssh-key -q -N "" -C "admin@fabric8.io"
+ssh-keygen -b 4096 -t rsa -f id_rsa -q -N "" -C "admin@fabric8.io"
 cd ..
 
-echo "Gen keys for jenkins, sonar users"
+echo ">> Genenate new keys for jenkins & sonar users"
 mkdir gerrit-users-ssh-keys
 cd gerrit-users-ssh-keys
-ssh-keygen -b 4096 -t rsa -f jenkins-master-ssh -q -N "" -C "jenkins@fabric8.io"
-ssh-keygen -b 4096 -t rsa -f sonarqube-ssh -q -N "" -C "sonar@fabric8.io"
+ssh-keygen -b 4096 -t rsa -f id_jenkins-rsa -q -N "" -C "jenkins@fabric8.io"
+ssh-keygen -b 4096 -t rsa -f id_sonar-rsa -q -N "" -C "sonar@fabric8.io"
 cd ..
 
-echo "Import keys generated"
+echo ">> Import keys generated"
 gofabric8 secrets -y
