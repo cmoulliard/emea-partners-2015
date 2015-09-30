f3542619de2fb6559667136d020cb95cf29ec8c3
Charles Moulliard <ch007m@gmail.com>
2015-09-30 19:19:15 +0200

    Change address of the gerrit server

--- demo/scripts/review.sh
+++ demo/scripts/review.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# Usage: ./review.sh <project_director> <repo_name>
+# Usage: ./review.sh <project_directory> <repo_name>
 # where <project_directory> is the location of a freshly checked out project from gogs
 # ideally, we'd automate the project import into gerrit, but this works fine for now
 
 PROJ_DIR=$1
 
 cd $PROJ_DIR
-git config user.name "Administrator"
-git config user.email "admin@company.com"
-git config remote.review.url  http://admin@gerrit.vagrant.local/$2
+git config user.name "admin"
+git config user.email "admin@fabric8.io"
+git config remote.review.url  http://admin@gerrit.vagrant.f8/$2
 git push review
 git config remote.review.push HEAD:refs/for/master
-curl -Lo .git/hooks/commit-msg http://gerrit.vagrant.local/tools/hooks/commit-msg
+curl -Lo .git/hooks/commit-msg http://gerrit.vagrant.f8/tools/hooks/commit-msg
 chmod +x .git/hooks/commit-msg
