946169111fde35445e95b5069c29bed17f88515b
admin <admin@fabric8.io>
2015-10-23 06:17:06 +0200

    Remove double space
    Change-Id: I465eef5a8b065ad29b86b7bd873a4c0c92596d1c

--- demo/scripts/review.sh
+++ demo/scripts/review.sh
@@ -9,7 +9,7 @@ PROJ_DIR=$1
 cd $PROJ_DIR
 git config user.name "admin"
 git config user.email "admin@fabric8.io"
-git config remote.review.url  http://admin@gerrit.vagrant.f8/$2
+git config remote.review.url http://admin@gerrit.vagrant.f8/$2
 git push review
 git config remote.review.push HEAD:refs/for/master
 curl -Lo .git/hooks/commit-msg http://gerrit.vagrant.f8/tools/hooks/commit-msg
